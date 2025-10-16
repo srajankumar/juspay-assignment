9aec5cc85be5d317e2004206466027d019d98792
srajankumar <kumarsrajan02@gmail.com>
2025-10-16 09:20:25 +0530

    add: dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
       <Button>hello world</Button>
+      <ModeToggle />
     </main>
   );
 }
